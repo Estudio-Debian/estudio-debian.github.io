29a45993ee059333ffdddf9448752b09334aec18
Raul Dipeas <github@rauldipeas.com.br>
2025-08-23 21:40:13 -0300

    ^inkscape

--- src/scripts/image/inkscape.sh
+++ src/scripts/image/inkscape.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e
 # shellcheck disable=SC2034
-INSTNAME='flowblade'
+INSTNAME='inkscape'
 # shellcheck disable=SC1090
 source <(curl -sSL https://estudio-debian.github.io/static/functions.sh)
 install_deb
